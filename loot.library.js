ecaaf605df1b08e6ecf07a7e523669757e2d1a70
Shirley Zhang <shirley.zhang@hotmail.com.au>
2016-12-10 20:13:52 +1100

    pls just check todo file. added score and something ? idk

--- loot.library.js
+++ loot.library.js
@@ -3,18 +3,18 @@ var Loot = {
 		width: 200,
 		height: 50,
 		treasure: [
-			{name: "Spider Leg"},
-			{name: "Spider Steak"},
-			{name: "Silk"}
+			{name: "Spider Leg",score: 400},
+			{name: "Spider Steak",score: 600},
+			{name: "Silk",score: 200}
 		]
 	},
 	"fridge": {
 		width: 50,
 		height: 100,
 		treasure: [
-			{name: "Stale bread"},
-			{name: "Bottle of water"},
-			{name: "Mouldy oranges"}
+			{name: "Stale bread",score: 300},
+			{name: "Bottle of water",score: 900},
+			{name: "Mouldy oranges",score: 100}
 		]
 	}
 }
--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 // initialising values
 var width = window.innerWidth;
-var height = width / 2;
+var height = 600;
 var ctx = canvas.getContext("2d");
 
 // Constants
+var BGB_OFFSET = 20;
+var BGF_OFFSET = 150;
+var BGM_OFFSET = 250;
+var LOOT_TIMER = 150;
 var ONE_FRAME_TIME = 1000 / 60 ;
-var PLAYER_RIGHT_BOUNDARY = width/2 + 100;
 var PLAYER_LEFT_BOUNDARY = width/2;
-var BACKBG_OFFSET = 80;
-var MIDBG_OFFSET = 250;
-var FORBG_OFFSET = 150;
 var PLAYER_OFFSET = 150;
+var PLAYER_RIGHT_BOUNDARY = width/2 + 100;
 
 
-var player = {width: 50};
+var player = {width: 50,looting:0};
+var enviro = {lootAnimation:0};
 // Background (back/mid/front)
-var bgb = {width: 2173}
-var bgm = {width: 600}
-var bgf = {width: 1526}
+var bgb = {width: 2173};
+var bgm = {width: 600};
+var bgf = {width: 1526};
 var bgbImg;
 var bgfImg;
 
@@ -29,6 +31,8 @@ window.onkeyup = function(e) {keys[e.keyCode]=false;}
 window.onkeydown = function(e) {keys[e.keyCode]=true;}
 
 var timeScore = 0;
+var time = 0;
+var score = 0;
 
 init();
 
@@ -43,7 +47,7 @@ function init() {
 	canvas.width  = width
 	canvas.height = height;
 	canvas.imageSmoothingEnabled = true;
-
+	
 	player.x = width/2; // Init player
 	player.y = PLAYER_OFFSET;
 	player.xspeed = 15;
@@ -55,7 +59,7 @@ function init() {
 	bgfImg.src = 'img/bgf.png';
 
 	// Setting bg height stagger for parallax effect
-	bgb.y = BACKBG_OFFSET; bgm.y = MIDBG_OFFSET; bgf.y = FORBG_OFFSET;
+	bgb.y = BGB_OFFSET; bgm.y = BGM_OFFSET; bgf.y = BGF_OFFSET;
 	// Each bg image has 3 instances to enable smooth scrolling
 	// This is initialising offset for each
 	bgb.x1 = 0; bgm.x1 = 0; bgf.x1 = 0;
@@ -78,11 +82,22 @@ function drawBackground() {
 	ctx.fillRect(0,0, width, 150);
 }
 
-function drawGame(){
+function sprite(x, y, srcImg){
+	this.x = x;
+	this.y = y;
+	this.srcImg = srcImg;
 
+	this.draw = function(ctx){
+		ctx.save();
+
+		ctx.restore();
+	}
+}
+
+function drawGame(){
+	ctx.save();
 	ctx.translate(0, canvas.height);
 	ctx.scale(1, -1);
-
 	drawBackground();
 
 	ctx.fillStyle = "green";
@@ -90,16 +105,30 @@ function drawGame(){
 		ctx.fillRect(lootNodes[i].x,lootNodes[i].y, lootNodes[i].nodeInfo.width, lootNodes[i].nodeInfo.height);
 	}
 
-	ctx.fillStyle = "grey";
+	if (player.looting != 0) {
+		ctx.fillStyle = "#b7b7b7";
+	} else {
+		ctx.fillStyle = "grey";
+	}
 	ctx.fillRect(player.x,player.y,player.width,100);
-	ctx.stroke();
 
-	ctx.translate(0, canvas.height);
-	ctx.scale(1, -1);
+	// ctx.translate(0, canvas.height);
+	// ctx.scale(1, -1);
+	ctx.restore();
 
 	ctx.fillStyle = "white";
 	ctx.font = "20px sans-serif";
-	ctx.fillText(timeScore, 10, 20);
+	ctx.fillText(time+score, 10, 20);
+
+	// Cancelling atm because too confusing
+	// if (enviro.lootAnimation != 0) {
+	// 	ctx.fillStyle = "grey";
+	// 	ctx.beginPath();
+	// 	ctx.lineWidth = 0;
+	// 	ctx.arc(width/2 +23, PLAYER_OFFSET + 100, 20, 0, (enviro.lootAnimation/100) * Math.PI);
+	// 	ctx.fill();
+	// }
+	
 }
 
 function isAtLootNode() {
@@ -114,8 +143,8 @@ function isAtLootNode() {
 
 		var lootWindowStart = currNode.x;
 		var lootWindowEnd = currNode.x + currNode.nodeInfo.width;
-		if (lootWindowStart < playerWindowStart && 
-				playerWindowEnd < lootWindowEnd) {
+		if (lootWindowStart < playerWindowEnd && 
+				playerWindowStart < lootWindowEnd) {
 			return i;
 		}
 	}
@@ -132,7 +161,9 @@ function moveObject(lootNode, property, speed) {
 }
 
 function moveBackground(obj, property, speed) {
-	obj[property] -= speed;
+	if (speed != 0) {
+		obj[property] -= speed;
+	}
 	var num = 0 - obj.width;
 	if (obj[property] < num ) {
 		obj[property] = width;
@@ -142,7 +173,7 @@ function moveBackground(obj, property, speed) {
 function updateGame() {
 	// Update score
 	if (player.xspeed > 5) {
-		timeScore+=1;
+		time+=1;
 	}
 
 	updateLootNodes();
@@ -155,6 +186,21 @@ function updateLooting() {
 	var lootingThis = isAtLootNode();
 	if (player.xspeed < 5 && lootingThis != -1) {
 		console.log("SWEET, SWEET LOOT");
+		player.looting += 1;
+		if (player.looting >= LOOT_TIMER) {
+			player.looting = 0;
+			var maxNum = lootNodes[lootingThis].nodeInfo.treasure.length;
+			// Grant a random loot
+			var item = Math.round(Math.random(new Date()) * (maxNum - 1));
+			score+=lootNodes[lootingThis].nodeInfo.treasure[item].score;
+		}
+		// enviro.lootAnimation+=1;
+		// if (enviro.lootAnimation/100 == 2) {
+		// 	enviro.lootAnimation = 0;
+		// }
+	} else {
+		player.looting = 0;
+		// enviro.lootAnimation = 0;
 	}
 }
 
@@ -163,7 +209,7 @@ function updateLootNodes() {
 	var lootMin = 0;
 
 	// Loot generation
-	if (timeScore % 200 == 0) {
+	if (time % 200 == 0) {
 		var lootRand = Math.random(new Date()) * (lootMax - lootMin) + lootMin;
 		var lootType = {};
 		if (lootRand > 15) {
@@ -187,7 +233,8 @@ function updateBackground() {
 	// keys[32] = Spacebar
 	if (keys[32] && player.xspeed > 0)	{
 		player.xspeed-=keymovepower;
-	} else if (keys[32] && player.xspeed < 0) {
+		
+	} else if (keys[32] && player.xspeed <= 0) {
 		player.xspeed = 0;
 	} else if (player.xspeed < 10) {
 		player.xspeed+=keymovepower;
